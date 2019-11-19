b0540ee8726729e811a0fa24f0c4e08121b20084
Felipe Pino <felipepinoredes@gmail.com>
2019-11-19 13:13:33 -0300

    primer commit

---
+++ app/Http/Controllers/EjemploController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class EjemploController extends Controller 
+{
+    public function inicio() {
+
+        return "Estás en el inicio del sitio";
+    }
+}
